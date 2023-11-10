f2f9c736ce432adaf9acdbaf719edf00bb359427
Klaudiusz Kołtuński <koltunski.klaudiusz@gmail.com>
2023-11-10 15:02:42 +0100

    data base connection added

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 02.10.2023
 Init commit
 
-05.10.2023
+05.11.2023
 - theme updated
 - application main icon updated (
     link to icon and author 
     <a href="https://www.flaticon.com/free-icons/climbing" title="climbing icons">Climbing icons created by monkik - Flaticon</a>
 )
 
-08.10.2023
+08.11.2023
 - login screen added (
     references:
     <a href="https://www.flaticon.com/free-icons/eye" title="eye icons">Eye icons created by DinosoftLabs - Flaticon</a>
@@ -16,3 +16,5 @@ Init commit
 - some generic composable that can be used in register screen added
 - register screen added
 - navigator composable added
+
+10.11. 2023
---
+++ project/app/src/main/java/com/example/project/database/DataBase.kt
@@ -0,0 +1,31 @@
+package com.example.project.database
+
+import android.os.StrictMode
+import java.sql.Connection
+import java.sql.DriverManager
+import java.sql.PreparedStatement
+import java.sql.ResultSet
+
+
+object DataBase {
+    @JvmStatic
+    fun executeQuerry(query: String): ResultSet {
+        val connection = getConnection()
+        val stmt: PreparedStatement = connection.prepareStatement(query)
+        return stmt.executeQuery()
+    }
+
+    private fun getConnection() : Connection {
+        Class.forName("com.mysql.jdbc.Driver")
+
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+
+        val connectionString = "jdbc:mysql://192.168.1.12:3306/engineeringthesis"
+        val username = "root"
+        val password = "root"
+
+        return DriverManager.getConnection(connectionString, username, password)
+    }
+}
+
